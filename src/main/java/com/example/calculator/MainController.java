c3fece97d834eca5389a22ead2d308451647cf24
Bohdan Bilyk <race1001@ukr.net>
2022-09-11 09:49:27 +0200

    First commit

---
+++ src/main/java/com/example/calculator/MainController.java
@@ -0,0 +1,46 @@
+package com.example.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Label Screen;
+
+    @FXML
+    private Button clear_btn;
+
+    @FXML
+    private Button comma_btn;
+
+    @FXML
+    private Button divide_btn;
+
+    @FXML
+    private Button minus_btn;
+
+    @FXML
+    private Button mult_btn;
+
+    @FXML
+    private Button percent_btn;
+
+    @FXML
+    private Button plus_btn;
+
+    @FXML
+    private Button plus_minus_btn;
+
+    @FXML
+    private Button res_btn;
+
+    @FXML
+    void addNumber(ActionEvent event) {
+
+    }
+
+}
+
